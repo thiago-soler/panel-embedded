b3682b775713c67d092a218421635e26613bdd6c
Thiago Soler <tsoler@uolinc.com>
2016-04-05 20:46:20 -0300

    Criando estrutura de painel responsiva utilizando flexbox. Refatoracao de estrutura criada no projeto panel-example-a.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,19 @@
 'use strict';
 
-$.ajax({url: "includes/nav.html", context: document.body}).done(function(data) {
+$.ajax({url: "includes/nav-theme-1.html", context: document.body}).done(function(data) {
 
 	$('#panel-nav .block').html(data);
+
+	$('#panel-nav .item-1').click(function(e) {
+		e.preventDefault();
+		$('#panel-nav .item-1').removeClass('selected');
+		$('#panel-nav .item-1').removeClass('active');
+		if ($(this).is('.list') === true) {
+			$(this).addClass('active');
+		} else {
+			$(this).addClass('selected');
+		}
+	});
 
 });
 
